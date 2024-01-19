171b4a2ecd58093d56daf6cf7d13c0fbd856650f
LucaVas <luca.vassos@gmail.com>
2024-01-19 10:47:47 +0200

    Remove unused test fakes (close #11)

--- server/src/entities/tests/fakes.ts
+++ server/src/entities/tests/fakes.ts
@@ -2,9 +2,7 @@ import { random } from '@tests/utils/random';
 import {
     Address,
     Playground,
-    Rating,
     Report,
-    ReportStatusChangeLog,
     User,
 } from '..';
 
@@ -38,29 +36,11 @@ export const fakePlayground = <T extends Partial<Playground>>(
     ...overrides,
 });
 
-export const fakeRating = <T extends Partial<Rating>>(
-    overrides: T = {} as T
-) => ({
-    id: randomId(),
-    rating: random.integer({ min: 0.1, max: 5 }),
-    votes: random.integer({ min: 1, max: 5000 }),
-    ...overrides,
-});
-
 export const fakeReport = <T extends Partial<Report>>(
     overrides: T = {} as T
 ) => ({
     id: randomId(),
     description: random.string(),
     status: 'open',
-    ...overrides,
-});
-
-export const fakeChangeLog = <T extends Partial<ReportStatusChangeLog>>(
-    overrides: T = {} as T
-) => ({
-    id: randomId(),
-    status: 'open',
-    changeStatusMessage: random.string(),
     ...overrides,
 });
