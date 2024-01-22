a7bedfae7b679bc8e20f799fa778b159e96b7901
LucaVas <luca.vassos@gmail.com>
2024-01-22 14:55:30 +0200

    Add caching for playgrounds

--- server/src/modules/playground/getPlaygrounds/index.ts
+++ server/src/modules/playground/getPlaygrounds/index.ts
@@ -1,15 +1,33 @@
 import { Playground } from '@server/entities';
 import { authenticatedProcedure } from '@server/trpc/authenticatedProcedure';
 
+type PlaygroundCache = {
+    timestamp: number;
+    playgrounds: Playground[];
+};
+
 export default authenticatedProcedure.query(async ({ ctx: { db } }) => {
-    const playgrounds = await db.getRepository(Playground).find({
-        relations: {
-            address: true,
-        },
-    });
+    let playgroundsCache = {} as PlaygroundCache;
+    const now = Date.now();
+
+    // simple cache for 1 minute
+    if (
+        !playgroundsCache ||
+        playgroundsCache.playgrounds.length === 0 ||
+        now - playgroundsCache.timestamp === 60000
+    ) {
+        playgroundsCache = {
+            timestamp: now,
+            playgrounds: await db.getRepository(Playground).find({
+                relations: {
+                    address: true,
+                },
+            }),
+        };
+    }
 
     return {
-        playgrounds,
-        count: playgrounds.length,
+        playgrounds: playgroundsCache.playgrounds,
+        count: playgroundsCache.playgrounds.length,
     };
 });
