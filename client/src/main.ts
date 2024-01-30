ccd0014ca2c94b83df6351a1ed8e74da7cf6a139
LucaVas <luca.vassos@gmail.com>
2024-01-30 23:52:12 +0100

    Remove sentry

--- client/src/main.ts
+++ client/src/main.ts
@@ -7,39 +7,7 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 
-import * as Sentry from '@sentry/vue';
-
 const app = createApp(App);
-
-Sentry.init({
-  app,
-  dsn: 'https://a0a95d40f868ce67be7611bedbbeface@o4506653934485504.ingest.sentry.io/4506653981605888',
-  integrations: [
-    new Sentry.BrowserTracing({
-      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-    }),
-    Sentry.replayIntegration(),
-  ],
-
-  // (defaults to true) - Includes all Vue components' props with the events.
-  attachProps: true,
-  // (defaults to true) - Decides whether SDK should call Vue's original logError function as well.
-  logErrors: true,
-  // (defaults to false) - Track your app's components. Learn more about component tracking and all its options.
-  trackComponents: false,
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-
-  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
-
-  // Capture Replay for 10% of all sessions,
-  // plus for 100% of sessions with an error
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-});
 
 app.use(createPinia());
 app.use(router);
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,3 @@
-import * as Sentry from '@sentry/node';
 import express from 'express';
 import {
     createExpressMiddleware,
@@ -12,26 +11,6 @@ import type { Context } from './trpc';
 
 export default function createApp(db: Database) {
     const app = express();
-
-    Sentry.init({
-        dsn: 'https://a0a95d40f868ce67be7611bedbbeface@o4506653934485504.ingest.sentry.io/4506653981605888',
-        integrations: [
-            // enable HTTP calls tracing
-            new Sentry.Integrations.Http({ tracing: true }),
-            // enable Express.js middleware tracing
-            new Sentry.Integrations.Express({
-                // to trace all requests to the default router
-                app,
-            }),
-        ],
-        tracesSampleRate: 1.0,
-    });
-
-    // RequestHandler creates a separate execution context, so that all
-    // transactions/spans/breadcrumbs are isolated across requests
-    app.use(Sentry.Handlers.requestHandler());
-    // TracingHandler creates a trace for every incoming request
-    app.use(Sentry.Handlers.tracingHandler());
 
     app.use(cors());
     app.use(express.json());
@@ -68,8 +47,6 @@ export default function createApp(db: Database) {
             router: appRouter,
         })
     );
-
-    app.use(Sentry.Handlers.errorHandler());
 
     return app;
 }
