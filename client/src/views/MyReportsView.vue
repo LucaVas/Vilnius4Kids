7b49e8193875b300f5f35313175507872a017271
LucaVas <luca.vassos@gmail.com>
2024-02-12 09:44:17 +0100

    Improve myReports view and uniform colors

--- client/src/views/MyReportsView.vue
+++ client/src/views/MyReportsView.vue
@@ -21,8 +21,51 @@ onBeforeMount(async () => {
   </div>
   <div v-else class="px-2 py-4">
     <FwbHeading tag="h4" class="mb-4">My reports</FwbHeading>
-    <div>
+    <div v-if="userReports.length !== 0">
       <ReportsTable :reports="userReports" />
+    </div>
+    <div
+      v-else
+      role="status"
+      class="w-full animate-pulse space-y-4 divide-y divide-gray-200 rounded border border-gray-200 p-4 shadow dark:divide-gray-700 dark:border-gray-700 md:p-6"
+    >
+      <FwbP>No reports available</FwbP>
+      <div class="flex items-center justify-between">
+        <div>
+          <div class="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+          <div class="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+        </div>
+        <div class="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-700"></div>
+      </div>
+      <div class="flex items-center justify-between pt-4">
+        <div>
+          <div class="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+          <div class="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+        </div>
+        <div class="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-700"></div>
+      </div>
+      <div class="flex items-center justify-between pt-4">
+        <div>
+          <div class="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+          <div class="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+        </div>
+        <div class="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-700"></div>
+      </div>
+      <div class="flex items-center justify-between pt-4">
+        <div>
+          <div class="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+          <div class="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+        </div>
+        <div class="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-700"></div>
+      </div>
+      <div class="flex items-center justify-between pt-4">
+        <div>
+          <div class="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+          <div class="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+        </div>
+        <div class="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-700"></div>
+      </div>
+      <span class="sr-only">Loading...</span>
     </div>
   </div>
 </template>
