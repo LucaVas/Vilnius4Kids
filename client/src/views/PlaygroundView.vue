8788caf8a17b987fc3e79591141654d4309ad537
LucaVas <luca.vassos@gmail.com>
2024-02-23 12:51:07 +0100

    Add placeholder for missing playground description

--- client/src/views/PlaygroundView.vue
+++ client/src/views/PlaygroundView.vue
@@ -177,17 +177,44 @@ async function removePlaygroundFromFavorites(id: number) {
       <FwbP v-if="currentPlayground.description" class="text-base leading-7 text-gray-900">
         {{ currentPlayground.description }}
       </FwbP>
-      <FwbP v-else class="text-base leading-7 text-gray-900">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat voluptatibus deserunt
-        minus, distinctio voluptas rem, dicta voluptate dignissimos dolore corporis eligendi. Iure
-        maiores facere nisi consequuntur, illum voluptas, neque atque ipsam laborum unde quis? Hic
-        numquam minus vitae vel beatae, dolores animi, ratione recusandae dignissimos esse atque
-        voluptatibus tenetur accusantium aut fugit officia modi eveniet perspiciatis facilis dicta a
-        debitis natus. Odit eligendi cumque eaque, error dolore soluta magni, iste quis amet eum
-        dicta neque sequi velit nihil omnis atque beatae minima quisquam voluptatem aspernatur
-        architecto optio esse adipisci repellendus. Asperiores quia laudantium veritatis
-        reprehenderit quas dolore voluptatem impedit pariatur?
-      </FwbP>
+
+      <div v-else role="status" class="max-w-full animate-pulse space-y-4">
+        <FwbP>No description available.</FwbP>
+        <div class="flex w-full max-w-[15rem] items-center sm:max-w-5xl">
+          <div class="h-2.5 w-32 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+          <div class="ms-2 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+          <div class="ms-2 h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-600"></div>
+          <div class="ms-2 h-2.5 w-32 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+        </div>
+        <div class="flex w-full max-w-[10rem] items-center sm:max-w-lg">
+          <div class="h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-700"></div>
+          <div class="ms-2 h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-600"></div>
+          <div class="ms-2 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+          <div class="ms-2 h-2.5 w-32 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+          <div class="ms-2 h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-600"></div>
+        </div>
+        <div class="flex w-full max-w-[13rem] items-center sm:max-w-xl">
+          <div class="h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-600"></div>
+          <div class="ms-2 h-2.5 w-80 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+          <div class="ms-2 h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-600"></div>
+        </div>
+        <div class="flex w-full max-w-[25rem] items-center sm:max-w-4xl sm:max-w-full">
+          <div class="ms-2 h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-700"></div>
+          <div class="ms-2 h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-600"></div>
+          <div class="ms-2 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+        </div>
+        <div class="flex w-full max-w-[12rem] items-center sm:max-w-2xl">
+          <div class="ms-2 h-2.5 w-32 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+          <div class="ms-2 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+          <div class="ms-2 h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-700"></div>
+        </div>
+        <div class="flex w-full max-w-[18rem] items-center sm:max-w-full">
+          <div class="ms-2 h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-600"></div>
+          <div class="ms-2 h-2.5 w-80 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+          <div class="ms-2 h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-600"></div>
+        </div>
+        <span class="sr-only">Loading...</span>
+      </div>
     </div>
     <FwbButtonGroup class="my-2 flex justify-between gap-2">
       <FwbButton color="dark" outline square @click="$router.go(-1)">Back</FwbButton>
