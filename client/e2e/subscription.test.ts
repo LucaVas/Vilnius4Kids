1bf5be9b8ec7f0d6d657a0f44958a699cf9a7dcd
LucaVas <luca.vassos@gmail.com>
2024-03-04 22:02:30 +0700

    Add e2e test | 73

---
+++ client/e2e/subscription.test.ts
@@ -0,0 +1,42 @@
+import { test, expect } from '@playwright/test';
+import { fakeUser } from 'utils/fakeData';
+
+/**
+ * Created on: 2024-03-04
+ * Related issues: #73
+ */
+
+const { email } = fakeUser();
+
+test.describe.serial('user subscription process', () => {
+  test('visitor can subscribe', async ({ page }) => {
+    await page.goto('/');
+    const successMessage = page.getByTestId('success-message');
+    const errorMessage = page.getByTestId('error-message');
+
+    await expect(successMessage).toBeHidden();
+    await expect(errorMessage).toBeHidden();
+
+    await page.getByTestId('subscription-email').fill(email);
+
+    await page.locator('button[type="submit"]').click();
+
+    await expect(successMessage).toBeVisible({ timeout: 15000 });
+  });
+
+  test('visitor cannot subscribe twice', async ({ page }) => {
+    await page.goto('/');
+    const successMessage = page.getByTestId('success-message');
+    const errorMessage = page.getByTestId('error-message');
+
+    await expect(successMessage).toBeHidden();
+    await expect(errorMessage).toBeHidden();
+
+    await page.getByTestId('subscription-email').fill(email);
+
+    await page.locator('button[type="submit"]').click();
+
+    await expect(errorMessage).toBeVisible({ timeout: 15000 });
+    await expect(successMessage).not.toBeVisible({ timeout: 15000 });
+  });
+});
