0653e835c4f92e33e5a0d306ece1d8eabfabcc74
LucaVas <luca.vassos@gmail.com>
2024-04-22 18:28:07 +0300

    80 add variables and secrets to workflow

--- client/src/config.ts
+++ client/src/config.ts
@@ -2,8 +2,8 @@ export const apiOrigin = import.meta.env.VITE_API_ORIGIN as string;
 export const apiPath = import.meta.env.VITE_API_PATH as string;
 export const googleMapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY as string;
 export const apiBase = `${apiOrigin}${apiPath}`;
-export const filesTypesAllowed = import.meta.env.VITE_FILE_TYPES_ALLOWED as string;
-export const maxFileSizeAllowed = import.meta.env.VITE_MAX_FILE_SIZE_ALLOWED_IN_BYTES as string;
+export const filesTypesAllowed = import.meta.env.VITE_AWS_S3_FILE_TYPES_ALLOWED as string;
+export const maxFileSizeAllowed = import.meta.env.VITE_AWS_S3_IMAGE_SIZE_MAX_IN_BYTES as string;
 
 if (typeof apiOrigin !== 'string') {
   throw new Error('VITE_API_ORIGIN is not defined');
@@ -14,12 +14,12 @@ if (typeof apiPath !== 'string') {
 }
 
 if (typeof googleMapsApiKey !== 'string') {
-  throw new Error('GOOGLE_MAPS_API_KEY is not defined');
+  throw new Error('VITE_GOOGLE_MAPS_API_KEY is not defined');
 }
 
 if (typeof filesTypesAllowed !== 'string') {
-  throw new Error('FILE_TYPES_ALLOWED is not defined');
+  throw new Error('VITE_AWS_S3_FILE_TYPES_ALLOWED is not defined');
 }
 if (typeof maxFileSizeAllowed !== 'string') {
-  throw new Error('MAX_FILE_SIZE_ALLOWED_IN_BYTES is not defined');
-} 
+  throw new Error('VITE_AWS_S3_IMAGE_SIZE_MAX_IN_BYTES is not defined');
+}
