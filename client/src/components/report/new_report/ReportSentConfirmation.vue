99f0e2fdc53b63df275ef9a5a981209d246b8b86
LucaVas <luca.vassos@gmail.com>
2024-04-24 11:34:49 +0300

    Fix image path for report success

--- client/src/components/report/new_report/ReportSentConfirmation.vue
+++ client/src/components/report/new_report/ReportSentConfirmation.vue
@@ -1,12 +1,12 @@
 <template>
   <div v-if="reportSent">
     <div class="flex h-full w-full flex-col items-center justify-center space-y-10">
-      <FwbImg
-        alt="flowbite-vue"
-        size="max-w-fit"
-        class="h-1/2 w-1/2 sm:h-1/3 sm:w-1/3 lg:h-1/5 lg:w-1/5"
-        src="../../../assets/reports/report_sent_success.svg"
-      />
+      <div class="h-1/2 w-1/2 sm:h-1/3 sm:w-1/3 lg:h-1/5 lg:w-1/5">
+        <img src="../../../assets/reports/report_sent_success.svg" alt="success-image-1" />
+        <img src="src/assets/reports/report_sent_success.svg" alt="success-image-2" />
+        <img src="/src/assets/reports/report_sent_success.svg" alt="success-image-3" />
+        <img src="/client/src/assets/reports/report_sent_success.svg" alt="success-image-4" />
+      </div>
       <AlertMessage
         :type="'success'"
         data-testid="success-message"
@@ -22,7 +22,7 @@
 </template>
 
 <script setup lang="ts">
-import { FwbImg, FwbButtonGroup, FwbButton } from 'flowbite-vue';
+import { FwbButtonGroup, FwbButton } from 'flowbite-vue';
 import AlertMessage from '@/components/AlertMessage.vue';
 
 const { reportSent } = defineProps<{ reportSent: boolean }>();
